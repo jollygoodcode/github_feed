45c0cd9e1d235db23d22b41a7b35ec5ff060dc01
Winston <winston.yongwei@gmail.com>
2015-09-28 00:21:41 +0800

    First spec.

--- spec/github_feed_spec.rb
+++ spec/github_feed_spec.rb
@@ -1,5 +1,12 @@
 require_relative "../github_feed"
 
 RSpec.describe GithubFeed do
+  describe "#initialization" do
+    it "works" do
+      feed = GithubFeed.new("rails/rails")
 
+      expect(feed).to be_an_instance_of GithubFeed
+      expect(feed.repo_name).to eq "rails/rails"
+    end
+  end
 end
