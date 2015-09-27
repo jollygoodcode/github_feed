caee27f13c39dabee5b48c81f85c9fb5e20d7ce7
Winston <winston.yongwei@gmail.com>
2015-09-28 02:43:36 +0800

    Let's use doubles!

--- spec/github_feed_spec.rb
+++ spec/github_feed_spec.rb
@@ -11,21 +11,23 @@ RSpec.describe GithubFeed do
   end
 
   describe "#recent_comments" do
+    let(:fake_events) { [ double(:event, to_s: event_to_s) ] }
+    let(:event_to_s) do
+      <<-COMMENT
+27 Sep 2015
+sgrif made a comment on Issue #21785
+at https://github.com/rails/rails/pull/21785#issuecomment-143571730
+      COMMENT
+    end
+
     before do
-      expect(GithubEvent).to receive(:all).with("rails/rails") { JSON.parse(File.read("spec/fixtures/events.json")) }
+      expect(GithubEvent).to receive(:all).with("rails/rails") { fake_events }
     end
 
     it "prints from API" do
       feed = GithubFeed.new("rails/rails")
 
-      expected =
-        <<-COMMENT
-27 Sep 2015
-sgrif made a comment on Issue #21785
-at https://github.com/rails/rails/pull/21785#issuecomment-143571730
-        COMMENT
-
-      expect(feed.recent_comments).to match expected
+      expect(feed.recent_comments).to match event_to_s
     end
   end
 end
